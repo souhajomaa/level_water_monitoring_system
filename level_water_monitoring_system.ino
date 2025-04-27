e877ec4ac822b666d53a81229b8e13ffa4956ab9
souhajomaa <souhajomaa1412@gmail.com>
2025-04-27 15:33:48 +0100

    first commit

---
+++ level_water_monitoring_system.ino
@@ -0,0 +1,131 @@
+#include<LiquidCrystal_I2C_Hangul.h>
+#include <Wire.h>
+#include <WiFi.h>
+//#define BLYNK_PRINT Serial
+#include <BlynkSimpleEsp32.h>
+#define WIFI_SSID ""// ISIMM-enseignants !IsImm@20Ens23!
+#define WIFI_PASSWORD ""
+#define BLYNK_TEMPLATE_ID ""
+#define BLYNK_TEMPLATE_NAME "Système de surveillance du niveau deau"
+
+LiquidCrystal_I2C_Hangul lcd(0x27, 16, 2);
+
+char auth[] = "";
+char ssid[] = "";
+char pass[] = "";
+
+BlynkTimer timer;
+
+
+#define trig 12
+#define echo 13
+#define LED1 2
+#define LED2 4
+#define LED3 5
+#define relay 14
+#define buzzer 33
+
+
+
+int MaxLevel = 7;
+
+int Level1 = (MaxLevel * 75) / 100;//4.5
+int Level2 = (MaxLevel * 50) / 100;//3
+int Level3 = (MaxLevel * 25) / 100;//1.5
+
+void setup() {
+  Serial.begin(9600);
+  lcd.init();
+  lcd.backlight();
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+  pinMode(relay, OUTPUT);
+  digitalWrite(relay, HIGH);
+  Blynk.begin(auth, ssid, pass, "blynk.cloud", 80);
+
+  lcd.setCursor(0, 0);
+  lcd.print("Niveau d'eau:");
+  lcd.setCursor(4, 1);
+  lcd.print("contrôle");
+  delay(4000);
+  lcd.clear();
+
+  
+ timer.setInterval(100L, ultrasonic);
+}
+
+
+void ultrasonic() {
+  digitalWrite(trig, LOW);
+  delayMicroseconds(4);
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+  long t = pulseIn(echo, HIGH);
+  int distance = t / 29 / 2;
+
+ int blynkDistance = (distance - MaxLevel) * -1;
+ if (distance <= MaxLevel) {
+ Blynk.virtualWrite(V0, blynkDistance);
+  } else {
+    Blynk.virtualWrite(V0, 0);
+  }
+  lcd.setCursor(0, 0);
+  lcd.print("Niveau:");
+
+  if (Level1 <= distance) {
+    lcd.setCursor(8, 0);
+    lcd.print("Low");
+    lcd.print("      ");
+    digitalWrite(LED1, HIGH);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, LOW);
+    digitalWrite(buzzer, LOW);
+   Serial.println(distance);
+    
+  } else if (Level2 <= distance && Level1 > distance) {
+    lcd.setCursor(8, 0);
+    lcd.print("Medium");
+    lcd.print("      ");
+    digitalWrite(LED1,LOW);
+    digitalWrite(LED2, HIGH);
+    digitalWrite(LED3, LOW);
+    digitalWrite(buzzer, LOW);
+     Serial.println(distance);
+
+  } else if (Level3 <= distance && Level2 > distance) {
+    lcd.setCursor(8, 0);
+    lcd.print("High");
+    lcd.print("      ");
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, HIGH);
+     digitalWrite(buzzer, HIGH);
+    delay(1000);
+    Serial.println(distance);
+    
+ 
+}
+
+
+BLYNK_WRITE(V1) {
+  bool Relay = param.asInt();
+ if (Relay == 1) {
+    digitalWrite(relay, LOW);
+    lcd.setCursor(0, 1);
+    lcd.print("Moteur est ON ");
+  } else {
+    digitalWrite(relay, HIGH);
+    lcd.setCursor(0, 1);
+    lcd.print("Moteur est OFF");
+  }
+}
+
+void loop(){
+  Blynk.run();
+  timer.run();
+}
